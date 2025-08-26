b42e425f5c2401295de4f6e4b18fcaac9b14c4dc
Jhay <your.email@example.com>
2025-08-26 09:23:32 -0400

    Initial React Project

---
+++ vite-project/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "c8488aeb8a2ab60fc0c61495c055786c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
